dd50e5ae2d38007b168ab78c1eae446ce6e637db
Jacqueline Shay <Jacqueline.A.Shay@gmail.com>
2019-10-01 07:12:11 -0500

    reorganize pandas_series

--- pandas_series.py
+++ pandas_series.py
@@ -73,10 +73,31 @@ id_of_longest
 longest_string = fruit_names[id_of_longest]
 longest_string
 
+# Write the code to get the longest string from the fruits series.
+fruit_names = fruits.unique()
+fruit_names = pd.Series(fruit_names)
+length_of_longest_string = fruit_names.str.len().max() # the length of the longest string
+index_of_longest_string = fruit_names.str.len().idxmax() # the index of the longest string
+longest_string = fruit_names[index_of_longest_string]
+
+print("The longest string in the list of fruits is", longest_string, "and it has", length_of_longest_string, "letters")
+
+# another approach to handle multiple longest strings
+length_of_longest_string = fruit_names.str.len().max() # the length of the longest string
+length_of_longest_string
+fruit_names[fruit_names.str.len() == length_of_longest_string]
 
 # Find the fruit(s) with 5 or more letters in the name.
+more_than_five_letters = fruits.apply(lambda x: len(x) >= 5)
+fruits[more_than_five_letters]
+
+# nice and short
+fruits[fruits.str.len() > 5]
+
 more_than_five = fruits.apply(lambda x: len(x) >= 5)
 fruits[more_than_five]
+
+
 
 # Capitalize all the fruit strings in the series.
 fruits.str.capitalize()
@@ -86,12 +107,45 @@ counts_of_a = fruit_names.str.count("a")
 list(zip(fruit_names, counts_of_a))
 
 
+# Count the letter "a" in all the fruits (use string vectorization)
+counts_of_a = fruit_names.str.count("a")
+counts_of_a
+fruit_names
+counts_of_a
+
+list(zip(fruit_names, counts_of_a))
+
+# another approach to output 2 series 
+for i, fruit in enumerate(fruit_names):
+    print(fruit, "has", counts_of_a[i], " number of 'a' characters.")
+
+
+df = pd.DataFrame(list(zip(fruit_names, counts_of_a)))
+df.columns = ["fruit_name", "count_of_a"]
+df
+
+
+
+
+
 
 # Output the number of vowels in each and every fruit.
 import re
 df = fruits.str.extractall('(?i)(?P<vowels_count>[aeiou])').groupby(level=0).count()
 df['fruit'] = fruits
 df.drop_duplicates()
+
+
+# Use the .apply method and a lambda function to fin d the fruit(s) containing two or more "o" letters in the name.
+with_two_or_more_o = fruit_names.apply(lambda x: x.count("o") >= 2)
+fruit_names[with_two_or_more_o]
+
+# Same operation using indexing and subsetting (instead of .apply and lambda)
+fruits[fruits.str.count("o") >= 2]
+
+
+
+
 # df.drop(['fruit_names'], axis = 1)
 
 def count_vowels(fruits):
@@ -117,6 +171,28 @@ fruits[fruits.str.contains("berry")]
 fruits[fruits.apply(lambda x: 'apple' in x)]
 # Which fruit has the highest amount of vowels?
 fruits[fruits.apply(count_vowels).idxmax()]
+
+
+
+# Write the code to get only the fruits containing "berry" in the name
+contains_berry = fruits.apply(lambda x: "berry" in x)
+fruits[contains_berry]
+
+# or we can use .str.contains
+fruits[fruits.str.contains("berry")]
+
+# Write the code to get only the fruits containing "apple" in the name
+contains_apple = fruits.str.contains("apple")
+fruits[contains_apple]
+
+# Which fruit has the highest amount of vowels?
+# get the highest vowel count first
+id_of_max = fruits.apply(count_vowels).idxmax() # gives us the index value of the highest vowel count
+fruits[id_of_max]
+
+
+
+====================================== have to revisit
 
 mon = pd.Series(['$796,459.41', '$278.60', '$482,571.67', '$4,503,915.98', '$2,121,418.3', '$1,260,813.3', '$87,231.01', '$1,509,175.45', '$4,138,548.00', '$2,848,913.80', '$594,715.39', '$4,789,988.17', '$4,513,644.5', '$3,191,059.97', '$1,758,712.24', '$4,338,283.54', '$4,738,303.38', '$2,791,759.67', '$769,681.94', '$452,650.23'])
 mon = mon.str.strip('$')
@@ -243,95 +319,3 @@ top
 top.plot(kind = 'bar', figsize = (5,5), title = 'Top 6 Repeated Letters')
 
 
-======= Ryan's
-
-import pandas as pd 
-
-fruits = pd.Series(["kiwi", "mango", "strawberry", "pineapple", "gala apple", "honeycrisp apple", "tomato", "watermelon", "honeydew", "kiwi", "kiwi", "kiwi", "mango", "blueberry", "blackberry", "gooseberry", "papaya"])
-
-fruits.describe()
-
-#  Run the code necessary to produce only the unique fruit names.
-fruits.unique()
-
-# Determine how many times each value occurs in the series.
-frequencies = fruits.value_counts()
-frequencies
-
-# Determine the most frequently occurring fruit name from the series.
-most_frequent = frequencies.max() 
-fruit = frequencies.idxmax()
-print("Most frequently occuring fruit is", fruit, "and it shows up", most_frequent, "times" )
-
-# Determine the least frequently occurring fruit name from the series.
-least_frequent = fruits.value_counts().min() # min gives us back the lowest value
-
-# fruits.value_counts()[fruits.value_counts() == least_frequent] is the same as below
-# indexing and subsetting 
-frequencies = fruits.value_counts()
-frequencies[frequencies == least_frequent]
-
-# Write the code to get the longest string from the fruits series.
-fruit_names = fruits.unique()
-fruit_names = pd.Series(fruit_names)
-length_of_longest_string = fruit_names.str.len().max() # the length of the longest string
-index_of_longest_string = fruit_names.str.len().idxmax() # the index of the longest string
-longest_string = fruit_names[index_of_longest_string]
-
-print("The longest string in the list of fruits is", longest_string, "and it has", length_of_longest_string, "letters")
-
-# another approach to handle multiple longest strings
-length_of_longest_string = fruit_names.str.len().max() # the length of the longest string
-length_of_longest_string
-fruit_names[fruit_names.str.len() == length_of_longest_string]
-
-# Find the fruit(s) with 5 or more letters in the name.
-more_than_five_letters = fruits.apply(lambda x: len(x) >= 5)
-fruits[more_than_five_letters]
-
-# nice and short
-fruits[fruits.str.len() > 5]
-
-# Capitalize all the fruit strings in the series.
-fruits.str.capitalize()
-
-# Count the letter "a" in all the fruits (use string vectorization)
-counts_of_a = fruit_names.str.count("a")
-counts_of_a
-fruit_names
-counts_of_a
-
-list(zip(fruit_names, counts_of_a))
-
-# another approach to output 2 series 
-for i, fruit in enumerate(fruit_names):
-    print(fruit, "has", counts_of_a[i], " number of 'a' characters.")
-
-
-df = pd.DataFrame(list(zip(fruit_names, counts_of_a)))
-df.columns = ["fruit_name", "count_of_a"]
-df
-
-
-# Use the .apply method and a lambda function to fin d the fruit(s) containing two or more "o" letters in the name.
-with_two_or_more_o = fruit_names.apply(lambda x: x.count("o") >= 2)
-fruit_names[with_two_or_more_o]
-
-# Same operation using indexing and subsetting (instead of .apply and lambda)
-fruits[fruits.str.count("o") >= 2]
-
-# Write the code to get only the fruits containing "berry" in the name
-contains_berry = fruits.apply(lambda x: "berry" in x)
-fruits[contains_berry]
-
-# or we can use .str.contains
-fruits[fruits.str.contains("berry")]
-
-# Write the code to get only the fruits containing "apple" in the name
-contains_apple = fruits.str.contains("apple")
-fruits[contains_apple]
-
-# Which fruit has the highest amount of vowels?
-# get the highest vowel count first
-id_of_max = fruits.apply(count_vowels).idxmax() # gives us the index value of the highest vowel count
-fruits[id_of_max]
